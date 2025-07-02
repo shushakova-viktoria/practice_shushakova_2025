fa3442d6a8f6bdd7eeac39f2a938a6bd3a4ee5a1
Viktoria <shushakova-va@yandex.ru>
2025-07-02 15:54:18 +0600

    completed task09

---
+++ practice2025/task09/task09.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+
+
+namespace task09
+{
+    class task09
+    {
+        static void Main(string[] args)
+        {
+            string dll = args[0];
+
+            Assembly ass = Assembly.LoadFrom(dll);
+
+            foreach(Type type in ass.GetTypes())
+            {
+                Console.WriteLine($"Имя класса: {type.Name}");
+
+                foreach (var method in type.GetMethods())
+                {
+                    Console.WriteLine($"Метод класса: {method.Name}");
+                    foreach (var par in method.GetParameters())
+                    {
+                        Console.WriteLine($"Параметр: {par.Name}, тип: {par.ParameterType.Name}");
+                    }
+                }
+
+                foreach(var attribute in type.GetCustomAttributes())
+                {
+                    Console.WriteLine($"Аттрибут класса: {attribute.GetType().Name}");
+                }
+
+                foreach(var constructor in type.GetConstructors())
+                {
+                    Console.WriteLine($"Конструкторы класса: {constructor.Name}");
+                    foreach(var par in constructor.GetParameters())
+                    {
+                        Console.WriteLine($"Параметр: {par.Name}, тип: {par.ParameterType.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
